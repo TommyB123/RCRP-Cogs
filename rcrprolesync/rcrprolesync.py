88aa15f865703a1f9170f90cbf6167c5ae86425c
TommyB <tommyb@frostgaming.com>
2025-05-09 07:51:45 -0700

    Update premium role sync to factor expiration

--- rcrprolesync/rcrprolesync.py
+++ rcrprolesync/rcrprolesync.py
@@ -101,7 +101,7 @@ class RCRPRoleSync(commands.Cog, name="RCRP Role Sync"):
                 if role_id == bannedrole:
                     await cursor.execute('SELECT masters.discordid FROM bans JOIN masters ON bans.MasterAccount = masters.id WHERE discordid != 0')
                 elif role_id == premiumrole:
-                    await cursor.execute("SELECT m.discordid FROM asettings a JOIN masters m ON m.id = a.sqlid  WHERE setting = 'ASET_PREMIUM' and m.discordid != 0")
+                    await cursor.execute("SELECT m.discordid FROM asettings a JOIN masters m ON m.id = a.sqlid  WHERE setting = 'ASET_PREMIUM' AND JSON_EXTRACT(extra, '$.PREMIUM_EXPIRATION') > UNIX_TIMESTAMP() and m.discordid != 0")
                 elif role_id == farole:
                     await cursor.execute("SELECT m.discordid FROM asettings a JOIN masters m on m.id = a.sqlid WHERE setting = 'ASET_FACTIONADMIN' and m.discordid != 0")
                 else:
